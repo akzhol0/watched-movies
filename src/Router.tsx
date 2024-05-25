129867745ca1a32a11d16827f55d14124538155e
Akzhol <ak.akzhol1603@gmail.com>
2024-05-25 22:28:27 +0500

    fixing bug

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,14 +1,26 @@
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./assets/styles/global.scss";
 import Footer from "./components/footer/Footer";
 import { ContextOverAll } from "./components/context/logic";
-import AppRouter from "./components/AppRouter";
+import MainPage from "./components/main-page/MainPage";
+import MoviePage from "./components/film-page/MoviePage";
+import ShowPage from "./components/film-page/ShowPage";
+import Login from "./components/auth/Login";
+import Register from "./components/auth/Register";
+import UnknownPage from "./components/unknown-page/UnknownPage";
 
 function Router() {
   return (
     <ContextOverAll>
       <BrowserRouter>
-        <AppRouter />
+        <Routes>
+          <Route path="/" element={<MainPage />}></Route>
+          <Route path="/movie/:title/:id" element={<MoviePage />}></Route>
+          <Route path="/show/:title/:id" element={<ShowPage />}></Route>
+          <Route path="/login" element={<Login />}></Route>
+          <Route path="/register" element={<Register />}></Route>
+          <Route path="*" element={<UnknownPage />}></Route>
+        </Routes>
         <Footer />
       </BrowserRouter>
     </ContextOverAll>
